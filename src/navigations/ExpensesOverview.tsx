1337f4ab672811835ae092f485219fa1d2683133
JoaoVitorLima242 <jlima0500@gmail.com>
2022-10-12 22:35:33 -0300

    add some style

--- src/navigations/ExpensesOverview.tsx
+++ src/navigations/ExpensesOverview.tsx
@@ -1,4 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { GlobalStyles } from "../assets/styles/GlobalStyles"
+import { Ionicons } from '@expo/vector-icons'
 
 // Screen
 import AllExpenses from "../screens/AllExpenses"
@@ -7,10 +9,41 @@ import RecentExpenses from "../screens/RecentExpenses"
 const { Navigator, Screen } = createBottomTabNavigator()
 
 const ExpensesOverview = () => {
+    const {
+        colors
+    } = GlobalStyles
+
     return (
-        <Navigator>
-            <Screen name="RecentExpenses" component={RecentExpenses}/>
-            <Screen name="AllExpenses" component={AllExpenses}/>
+        <Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: colors.primary500,
+                },
+                headerTintColor: 'white',
+                tabBarStyle: {
+                    backgroundColor: colors.primary500,
+                },
+                tabBarActiveTintColor: colors.accent500
+            }}
+        >
+            <Screen 
+                name="RecentExpenses" 
+                component={RecentExpenses}
+                options={{
+                    title: 'Recent Expenses',
+                    tabBarLabel: 'Recent',
+                    tabBarIcon: ({color, size}) => <Ionicons color={color} size={size} name='hourglass'/>
+                }}
+            />
+            <Screen 
+                name="AllExpenses" 
+                component={AllExpenses}
+                options={{
+                    title: 'Recent Expenses',
+                    tabBarLabel: 'Recent',
+                    tabBarIcon: ({color, size}) => <Ionicons color={color} size={size} name='calendar'/>
+                }}
+            />
         </Navigator>
     )
 }
