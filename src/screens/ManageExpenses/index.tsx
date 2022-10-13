c57a47fd7648b7e5b49eb8c63efe8f1b8889fe61
JoaoVitorLima242 <jlima0500@gmail.com>
2022-10-12 21:37:08 -0300

    create pages and adjust folders

---
+++ src/screens/ManageExpenses/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const ManageExpenses = () => {
+    return (
+        <View>
+            <Text>Manage Expenses Screen!</Text>
+        </View>
+    )
+}
+
+export default ManageExpenses
