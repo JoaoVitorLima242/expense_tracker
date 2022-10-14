6fdd41d54776d591452d5492d658de312ee5e7db
JoaoVitorLima242 <jlima0500@gmail.com>
2022-10-13 21:56:12 -0300

    add some style to navigation

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { GlobalStyles } from "../assets/styles/GlobalStyles";
 
 import ManageExpenses from "../screens/ManageExpenses";
 import ExpensesOverview from "./ExpensesOverview";
@@ -19,13 +20,20 @@ declare global {
 const Navigation = () => {
     return (
         <NavigationContainer>
-            <Navigator>
+            <Navigator
+                screenOptions={{
+                    headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+                    headerTintColor: 'white'
+                }}
+            >
                 <Screen 
                     name="ExpensesOverview" 
                     component={ExpensesOverview} 
                     options={{headerShown: false}}
                 />
-                <Screen name="ManageExpense" component={ManageExpenses}/>
+                <Screen name="ManageExpense" component={ManageExpenses} options={{
+                    presentation: 'modal'
+                }}/>
             </Navigator>
         </NavigationContainer>
     )
