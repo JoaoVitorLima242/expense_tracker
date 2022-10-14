65c222357e3f40aeb3871070428ae65004841e42
JoaoVitorLima242 <jlima0500@gmail.com>
2022-10-13 22:43:52 -0300

    adjusting header

--- src/screens/ManageExpense/index.tsx
+++ src/screens/ManageExpense/index.tsx
@@ -1,12 +1,21 @@
 import { ParamListBase, RouteProp } from "@react-navigation/native"
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { useLayoutEffect } from "react";
 import { Text, View } from "react-native"
 import { RootStackParamList } from "../../types";
 
 type Props = NativeStackScreenProps<RootStackParamList, 'ManageExpense'>;
 
-const ManageExpense = ({route}: Props) => {
+const ManageExpense = ({route, navigation}: Props) => {
     const editedExpenseId = route.params?.expenseId
+    const isEditing = !!editedExpenseId
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: isEditing ? 'Edit Expense' : 'Add Expense'
+        })
+    }, [isEditing, navigation])
+
 
     return (
         <View>
