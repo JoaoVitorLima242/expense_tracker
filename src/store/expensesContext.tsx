a9596b0f247ebcd2c93426bcbcb6f019684dbdd9
JoaoVitorLima242 <jlima0500@gmail.com>
2022-10-21 16:28:55 -0300

    create context

--- src/store/expensesContext.tsx
+++ src/store/expensesContext.tsx
@@ -1,4 +1,38 @@
 import { createContext, useReducer } from "react";
+import { Expense } from "../types";
+
+const DUMMY_EXPENSES: Expense[] = [
+    {
+        id: 'e1',
+        description: 'A pair of shoes',
+        amount: 59.99,
+        date: new Date('2022-04-22')
+    },
+    {
+        id: 'e2',
+        description: 'MacBook Pro M2',
+        amount: 1999.99,
+        date: new Date('2022-04-22')
+    },
+    {
+        id: 'e3',
+        description: 'The Shinning',
+        amount: 79.99,
+        date: new Date('2021-12-29')
+    },
+    {
+        id: 'e4',
+        description: 'Video Game',
+        amount: 279.99,
+        date: new Date('2021-03-19')
+    },
+    {
+        id: 'e5',
+        description: 'God of War',
+        amount: 179.99,
+        date: new Date('2021-01-09')
+    },
+]
 
 export const ExpensesContext = createContext({
     expenses: [],
@@ -13,14 +47,25 @@ const expensesReducer = (state: any, action: any) => {
             const id = new Date().toString() + Math.random().toString()
             return [{ id, ...action.payload }, ...state]
         case 'UPDATE':
+            const updatedExpenseIndex = state.findIndex(
+                expense => expense.id === action.payload.id
+            );
+
+            const updatedExpense = state[updatedExpenseIndex]
+            const updatedItem = {...updatedExpense, ...action.payload.data}
+            const updatedExpenses = [...state]
+            updatedExpenses[updatedExpenseIndex] = updatedItem
+
+            return updatedExpenses
         case 'DELETE':
+            return state.filter(expense => expense.id !== action.payload.id)
         default: 
             return state
     }
 }
 
 const ExpensesProvider = ({children}: any) => {
-    const [expensesState, dispatch] = useReducer(expensesReducer)
+    const [expenses, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES)
 
     const addExpense = (expenseData: any) => {
         dispatch({type: 'ADD', payload: expenseData})
@@ -34,9 +79,15 @@ const ExpensesProvider = ({children}: any) => {
         dispatch({type: 'DELETE', payload: {id, data: expenseData}})
     }
 
+    const value = {
+        expenses,
+        addExpense,
+        deleteExpense,
+        updateExpense
+    }
 
     return (
-        <ExpensesContext.Provider>
+        <ExpensesContext.Provider value={value}>
             {children}
         </ExpensesContext.Provider>
     )
