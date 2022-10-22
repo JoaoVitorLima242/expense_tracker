8fc090d6e9c56137a27b7b5336cb24722ddf40e4
JoaoVitorLima242 <jlima0500@gmail.com>
2022-10-21 23:15:33 -0300

    adjusting input styles

--- src/components/Manage/Form/index.tsx
+++ src/components/Manage/Form/index.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native"
+import { StyleSheet, View } from "react-native"
 import Input from "../Input"
 
 const ExpenseForm = () => {
--- src/components/Manage/Input/index.tsx
+++ src/components/Manage/Input/index.tsx
@@ -1,4 +1,5 @@
-import { TextInputProps, Text, TextInput, View } from "react-native"
+import { TextInputProps, Text, TextInput, View, StyleSheet, TextStyle, StyleProp } from "react-native"
+import { GlobalStyles } from "../../../assets/styles/GlobalStyles"
 
 type Props = {
     label?: string
@@ -6,12 +7,41 @@ type Props = {
 }
 
 const Input = ({label, textInputConfig}: Props) => {
+
+    let inputStyles: StyleProp<TextStyle> = [styles.input]
+
+    if (textInputConfig && textInputConfig.multiline) {
+        inputStyles.push(styles.inputMultiline)
+    }
+
     return (
-        <View>
-            {label && <Text>{label}</Text>}
-            <TextInput {...textInputConfig}/>
+        <View style={styles.container}>
+            {label && <Text style={styles.label}>{label}</Text>}
+            <TextInput style={inputStyles} {...textInputConfig}/>
         </View>
     )
 }
 
 export default Input
+
+const styles = StyleSheet.create({
+    container: {
+        marginHorizontal: 4,
+        marginVertical: 8
+    },
+    label: {
+        fontSize: 12,
+        color: 'white',
+        marginBottom: 4
+    },
+    input: {
+        backgroundColor: 'white',
+        padding: 6,
+        borderRadius: 6,
+        fontSiz: 18,
+        color: GlobalStyles.colors.primary700
+    },
+    inputMultiline: {
+        height: 100
+    }
+})
